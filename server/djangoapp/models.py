c7a5731b235695eb3aa0531096d071616ef33169
Rehtest <rehtesten@gmail.com>
2024-04-14 09:49:49 -0400

    Code cleanup

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -35,7 +35,7 @@ class CarModel(models.Model):
         default=SEDAN,
     )
     year = models.IntegerField(
-        default=2024, validators=[MinValueValidator(2015), MaxValueValidator(2024)]
+        default=2024, validators=[MinValueValidator(2015), MaxValueValidator(2023)]
     )
 
     def __str__(self):
