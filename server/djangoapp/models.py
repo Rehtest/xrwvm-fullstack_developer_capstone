6cceee6c9e1e30d4e77fa1a48827160a8e73a53e
Rehtest <rehtesten@gmail.com>
2024-04-14 08:20:28 -0400

    Add dealer list, dealer details and reviews

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -35,26 +35,8 @@ class CarModel(models.Model):
         default=SEDAN,
     )
     year = models.IntegerField(
-        default=2024, validators=[MinValueValidator(2015), MaxValueValidator(2023)]
+        default=2024, validators=[MinValueValidator(2015), MaxValueValidator(2024)]
     )
 
     def __str__(self):
         return self.name
-
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
